13c82cb696dae437eb505d7fcfa49e9bd26f29b8
Bilala14 <kanianabilamuntaz14@gmail.com>
2024-11-23 12:18:20 +0700

    mengubah koneksi

--- employee_manager/koneksi.php
+++ employee_manager/koneksi.php
@@ -2,7 +2,7 @@
 $host ="localhost";
 $username = "root";
 $password ="";
-$database ="is_62";
+$database ="uts_kania_nabila_muntaz_is62";
 
 $koneksi = mysqli_connect($host,$username,$password,$database);
 

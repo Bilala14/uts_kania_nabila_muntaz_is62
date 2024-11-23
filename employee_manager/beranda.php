6cb4b1d6c9526e73e746c49a9e0efb54f148b19f
Bilala14 <kanianabilamuntaz14@gmail.com>
2024-11-23 12:28:25 +0700

    mengubah beranda dan navbar

--- employee_manager/beranda.php
+++ employee_manager/beranda.php
@@ -17,7 +17,7 @@
        <h3>Employee Manager</h3>
     </div>
     <div class="card-body">
-      <h4>Selamat Datang</h4>
+      <h4>Selamat Datang Di Employee Manager</h4>
       <p></p>
     </div>
     </div>
--- employee_manager/navbar.php
+++ employee_manager/navbar.php
@@ -13,7 +13,7 @@
           <a class="nav-link active" aria-current="page" href="#"></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/employee_manager/indexcopy.php">Data Karyawan</a>
+          <a class="nav-link active" aria-current="page" href="/employee_manager/indexcopy.php"></a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#"></a>

245542a550967ca34a0b4033030fa64bb7319d36
Bilala14 <kanianabilamuntaz14@gmail.com>
2024-11-23 12:18:42 +0700

    mengubah navbar

--- employee_manager/navbar.php
+++ employee_manager/navbar.php
@@ -1,22 +1,22 @@
 <nav class="navbar navbar-expand-lg bg-danger-subtle">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">SIM</a>
+    <a class="navbar-brand" href="#">Employee Manager</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/is62_kania/Projek/index.php">Beranda</a>
+          <a class="nav-link active" aria-current="page" href="/uts_kania_nabila_muntaz_is62/employee_manager/indexcopy.php">Hal Utama</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Data Mahasiswa</a>
+          <a class="nav-link active" aria-current="page" href="#"></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/is62_kania/Projek/modul_jurusan/index.php">Data Jurusan</a>
+          <a class="nav-link active" aria-current="page" href="/employee_manager/indexcopy.php">Data Karyawan</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Data Dosen</a>
+          <a class="nav-link active" aria-current="page" href="#"></a>
         </li>
       </ul>
       <form class="d-flex" role="search">
